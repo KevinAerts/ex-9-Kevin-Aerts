4d0024e19e5c522a3784c5a8503d8c90dd93eee5
Kevin Aerts <r0579041@student.thomasmore.be>
2017-01-04 14:34:04 +0100

    inladen dependencies waardoor ik een http webserver kan opstarten. Dependencies staan niet in package.json maar werkt wel.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+app.use(bodyparser.json());
+
+app.listen(4567);
+
+console.log("server is opgestart...");
+
