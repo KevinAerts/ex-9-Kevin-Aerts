3404540a7cddf5a7e1078941a1d530ea9552aed4
Kevin Aerts <r0579041@student.thomasmore.be>
2017-01-04 14:40:31 +0100

    storage en validate van locaties aangemaakt.

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var bodyparser = require('body-parser');
 
+var dallocaties = require('./Storagelocaties.js');
+var validatelocaties = require('./Validatelocaties.js');
+
+
 var app = express();
 app.use(bodyparser.json());
 
-app.listen(4567);
 
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4567);
 console.log("server is opgestart...");
 
