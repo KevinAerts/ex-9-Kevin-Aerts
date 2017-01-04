57a11f92340e54d7c0cf62846945a03455c114ee
Kevin Aerts <r0579041@student.thomasmore.be>
2017-01-04 15:03:36 +0100

    ervoor gezorgd dat locaties kunnen opgevraagd worden met behulp van "GET" methode.

--- Storagelocaties.js
+++ Storagelocaties.js
@@ -1,7 +1,33 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var mongoose = require("mongoose");
+var locatieSchema = mongoose.Schema({
+            naam_drone: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            mac_address_drone: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            naam_locatie: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            beschrijving: {
+                type: String,
+                required: true
+            }
+});
+var Locatie = module.exports = mongoose.model('Locatie', locatieSchema);
 
+module.exports = {
 
+    AllLocaties : function(callback) {
+        Locatie.find(callback);
+    },
+    findLocaties : function(id, callback){
+        Locatie.find({naam_drone:id}, callback);
+    }
+};
--- main.js
+++ main.js
@@ -7,18 +7,30 @@ var validatelocaties = require('./Validatelocaties.js');
 
 
 var app = express();
+mongoose.connect('mongodb://localhost:27017/API');
 app.use(bodyparser.json());
 
+//------ alles van locaties ------
+// opvangen van een GET op /locaties
+app.get('/locaties', function (request, response) {
+    dallocaties.AllLocaties(function (err, locatie) {
+        if(err){
+            throw err;
+        }
+        response.send(locatie);
+    });
+});
 
-
-
-
-
-
-
-
-
-
+// opvangen van een GET op /locaties/:naam_drone
+app.get('/locaties/:id', function (request, response) {
+    dallocaties.findLocaties(request.params.id, function (err, locatie) {
+        if (locatie) {
+        response.send(locatie);
+    } else {
+        throw err;
+    }
+    });
+});
 
 
 
